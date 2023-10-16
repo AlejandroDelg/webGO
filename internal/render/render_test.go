2b10992483b1d2809fba3c56432b4f3d1bfc9e85
AlejandroDelg <alexdelga2013@gmail.com>
2023-10-16 22:05:38 +0200

    Test Render package AddDefaultData

---
+++ internal/render/render_test.go
@@ -0,0 +1,33 @@
+package render
+
+import (
+	"github.com/AlejandroDelg/webgo/internal/models"
+	"net/http"
+	"testing"
+)
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+	session.Put(r.Context(), "flash", "123")
+	result := AddDefaultData(&td, r)
+	if result.Flash != "123" {
+		t.Error("error adding dafault data")
+	}
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X_Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
---
+++ internal/render/setup_test.go
@@ -0,0 +1,32 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/AlejandroDelg/webgo/internal/config"
+	"github.com/AlejandroDelg/webgo/internal/models"
+	"github.com/alexedwards/scs/v2"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	gob.Register(models.Reservation{})
+	testApp.InProduction = false
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+	os.Exit(m.Run())
+}
